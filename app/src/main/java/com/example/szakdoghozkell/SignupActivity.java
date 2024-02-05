2b1bb061665b8d3ab6097e13c2edb37a1cad8322
TorokMarcell <marcell.torok21@gmail.com>
2024-02-05 23:35:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/szakdoghozkell/SignupActivity.java
@@ -0,0 +1,59 @@
+package com.example.szakdoghozkell;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.szakdoghozkell.databinding.ActivitySignupBinding;
+
+public class SignupActivity extends AppCompatActivity {
+    ActivitySignupBinding binding;
+    DatabaseHelper databaseHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        databaseHelper = new DatabaseHelper(this);
+        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.signupEmail.getText().toString();
+                String password = binding.signupPassword.getText().toString();
+                String confirmPassword = binding.signupConfirm.getText().toString();
+                if(email.equals("")||password.equals("")||confirmPassword.equals(""))
+                    Toast.makeText(SignupActivity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+                else{
+                    if(password.equals(confirmPassword)){
+                        Boolean checkUserEmail = databaseHelper.checkEmail(email);
+                        if(!checkUserEmail){
+                            Boolean insert = databaseHelper.insertData(email, password);
+                            if(insert){
+                                Toast.makeText(SignupActivity.this, "Signup Successfully!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),LgoinActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(SignupActivity.this, "Signup Failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(SignupActivity.this, "User already exists! Please login", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(SignupActivity.this, "Invalid Password!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        binding.loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LgoinActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
