305095a96b90c49c8656dea1a3430af5fd0c7ce7
TorokMarcell <marcell.torok21@gmail.com>
2024-02-06 19:08:49 +0100

    imageupload

--- app/src/main/java/com/example/szakdoghozkell/MainActivity.java
+++ app/src/main/java/com/example/szakdoghozkell/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.szakdoghozkell;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView =findViewById(R.id.imageView);
+        imageButton = findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(MainActivity.this)
+                        .crop()
+                        .compress(1024)
+                        .maxResultSize(1080, 1080)
+                        .start();
+                Intent intent = new Intent();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        imageView.setImageURI(uri);
     }
 }
